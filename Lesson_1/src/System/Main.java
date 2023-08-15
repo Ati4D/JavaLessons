f61d2da40e052f884fc3c7a6a1294771f5449b75
Ati4D <diominov228@gmail.com>
2023-08-15 16:00:38 +0300

    Homework_1

---
+++ Lesson_1/src/System/Main.java
@@ -0,0 +1,13 @@
+package System;
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+        out.println("Hello world!");
+        int a = 12;
+        int b = 15;
+        int c;
+        c = a+b;
+        out.println(c);
+    }
+}
